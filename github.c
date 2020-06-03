ed2c464e7b1edc6836f35a6ed461bb6a06586f1a
applyy100@gmail.com <applyy100@gmail.com>
2020-06-03 20:27:02 +0900

    calculate

--- github.c
+++ github.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 
 int main(){
+  int a,b;
   printf("hello world");
+  
+  printf("숫자두개 입력 : ");
+  scanf("%d%d",&a,&b);
+  
+  printf("%d+%d=%d",a,b,a+b);
+  printf("%d-%d=%d",a,b,a-b);
+  printf("%d/%d=%d",a,b,a/b);
+  printf("%d%%d=%d",a,b,a%b);
 }
